f029e9cd5de20be57dec5df427b448846b2d71c3
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-05 17:41:51 +0530

    Commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,16 @@ import TypingAnimation from "./TypingAnimation"
 
 const Intro = () => {
   return (
-    <section className='flex items-center min-h-[200px] h-auto bg-gray-800 text-white justify-evenly'>
-      <div>
-        <div>Hi, <br />I'm <span className='font-jersey text-[30px] text-yellow-500'>Harsh Master</span></div>
-        <div className='font-jersey'>Android Developer, Web Developer</div>
-      </div>
-      <div>
-        <img src={"/profile pic.png"} className='max-h-[300px] min-h-[150px]' alt="" />
+    <section className='flex flex-col justify-around items-center min-h-[400px] h-auto bg-black text-white'>
+      <div className='flex items-center justify-evenly w-full'>
+
+        <div>
+          <div>Hi, <br />I'm <span className='font-jersey text-[30px] text-yellow-500'>Harsh Master</span></div>
+          <div id="skills" className='font-jersey'>Android Developer, Web Developer</div>
+        </div>
+        <div>
+          <img src={"/profile pic.png"} className='max-h-[300px] min-h-[150px]' alt="" />
+        </div>
       </div>
 
       <TypingAnimation />
--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react'
 const TypingAnimation = () => {
 
   const [input, setInput] = useState("")
-  const [animationComplete, setAnimationComplete] = useState(false)
+  const [fadeIn, setFadein] = useState(false)
   const index = useRef(0)
 
   const handleInputChange = (e) => {
@@ -11,23 +11,39 @@ const TypingAnimation = () => {
   }
 
   useEffect(() => {
-    if (animationComplete) {
+    if (fadeIn) {
       const typingText = "Android Developer";
       const typingInterval = setInterval(() => {
         setInput(typingText.substring(0, index.current + 1));
         index.current++;
         if (index.current === typingText.length) clearInterval(typingInterval);
       }, 100);
-      return () => clearInterval(typingInterval);
+      return () => {
+        clearInterval(typingInterval)
+      };
     }
-  }, [animationComplete])
+  }, [fadeIn])
 
   return (
-    <div className='border-2 rounded-lg px-2 py-1 animate-fade' onAnimationEnd={() => { setAnimationComplete(true)}}>
-      <input type="text" className='bg-transparent outline-none' onChange={handleInputChange} value={input} autoFocus />
-      ↵ &#9166;
+    <div id='hi2' className='flex flex-row w-[20%] min-w-[200px] relative border-2 rounded-lg px-2 py-1 animate-fade' onAnimationEnd={() => { setFadein(true)}}>
+      <input type="text" className='w-full bg-transparent outline-none' onChange={handleInputChange} value={input} autoFocus />
+      <span>↵&#9166;</span>
     </div>
   )
 }
 
 export default TypingAnimation
+
+// const movingDiv = document.getElementById('hi2');
+// const stati = document.getElementById('hi');
+
+// // Initial and final positions
+// const initialPos = { x: movingDiv.getBoundingClientRect().x, y: hi2.getBoundingClientRect().y }; // Assuming initial position (150px, 100px) relative to container1
+// const finalPos = { x: stati.getBoundingClientRect().x, y: stati.getBoundingClientRect().y+40 }; // Assuming final position (150px, 100px) relative to container2
+
+// // Calculate translation values
+// const dx = finalPos.x - initialPos.x;
+// const dy = finalPos.y - initialPos.y;
+
+// // Apply transformation to move the div to the final position
+// movingDiv.style.transform = `translate(${dx}px, ${dy}px)`;
