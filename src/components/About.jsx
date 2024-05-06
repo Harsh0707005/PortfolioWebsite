82bfdead1d8d2acbdd38b8ae53c7a61cc6f87530
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-06 23:07:25 +0530

    Commit

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='bg-[#333333] text-white min-h-[500px]'>
+        
+    </section>
+  )
+}
+
+export default About
