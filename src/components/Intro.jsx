4f375d7c9f0d991e777b55c95ccf23816e1a457e
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-06 09:46:22 +0530

    Typing Animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,71 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import TypingAnimation from "./TypingAnimation"
+import React, { useEffect, useState } from 'react';
+import TypingAnimation from './TypingAnimation';
 
 const Intro = () => {
-  const [initialCoordinates, setInitialCoordinates] = useState({ x: 0, y: 0 })
-  const [finalCoordinates, setFinalCoordinates] = useState({ x: 0, y: 0 })
-  const [popId, setPopId] = useState("")
+  const [initialCoordinates, setInitialCoordinates] = useState({ x: 0, y: 0 });
+  const [finalCoordinates, setFinalCoordinates] = useState({ x: 0, y: 0 });
+  const [popId, setPopId] = useState('');
 
+  const handleAnimationComplete = ({ popId }) => {
+    const popDiv = document.getElementById(popId);
+    const skillsDiv = document.getElementById('skills');
+    const rect = popDiv.getBoundingClientRect();
+    setInitialCoordinates({
+      x: rect.x,
+      y: rect.y
+    });
+    // console.log(skillsDiv.offsetWidth)
+    setFinalCoordinates({
+      x: skillsDiv.getBoundingClientRect().x,
+      y: skillsDiv.getBoundingClientRect().y
+    });
+    setPopId(popId);
+  };
 
   useEffect(() => {
     if (initialCoordinates.x !== 0 && initialCoordinates.y !== 0) {
-
-      const skillsDiv = document.getElementById("skills");
-      let rect = skillsDiv.getBoundingClientRect();
-
-      setFinalCoordinates({
-        x: rect.x,
-        y: rect.y
-      })
-
-      console.log("Final Coordinates", finalCoordinates)
-    }
-
-
-  }, [initialCoordinates])
-
-  useEffect(() => {
-    if (finalCoordinates.x !== 0 && finalCoordinates.y !== 0) {
-      const dx = finalCoordinates.x - initialCoordinates.x;
+      const skillsDiv = document.getElementById('skills');
+      const dx = finalCoordinates.x - initialCoordinates.x + skillsDiv.offsetWidth + 5;
       const dy = finalCoordinates.y - initialCoordinates.y;
-
-
       const popup = document.getElementById(popId);
-      popup.style.transition = 'transform 1.5s ease'; // Slow and smooth transition
+      popup.style.transition = 'transform 1.5s ease';
       popup.style.transform = `translate(${dx}px, ${dy}px)`;
-      popup.addEventListener("transitionend", () => {
-        popup.style.display = "none"
-        document.getElementById("skills").innerHTML += popup.innerHTML
-      })
+      popup.addEventListener('transitionend', () => {
+        popup.style.transform = `translate(${initialCoordinates.x}px, ${initialCoordinates.y}px)`;
+        popup.style.display = 'none';
+        const skillsDiv = document.getElementById('skills');
+        let comma = (skillsDiv.offsetWidth === 0 || skillsDiv.innerHTML === '') ? '' : ', ';
+        skillsDiv.innerHTML += `${comma}${popup.innerHTML}`;
+      });
     }
-  }, [finalCoordinates])
-
-  const handleAnimationComplete = ({ newCoordinates, popId }) => {
-    // Receive coordinates from TypingAnimation
-    console.log("Intro", popId, "tempcoordinates", newCoordinates)
-    setInitialCoordinates(newCoordinates);
-    setPopId(popId);
-
-    // Now you can do whatever you want with the coordinates
-    console.log("Initial coordinates:", newCoordinates);
-  };
-
+  }, [finalCoordinates]);
 
   return (
     <section className='flex flex-col justify-around items-center min-h-[400px] h-auto bg-black text-white'>
       <div className='flex items-center justify-evenly w-full max-md:flex-col-reverse max-md:gap-8'>
-
-        <div>
-          <div>Hi, <br />I'm <span className='font-jersey text-[30px] text-yellow-500'>Harsh Master</span></div>
-          <div id="skills" className='font-jersey'></div>
+        <div className='flex flex-col'>
+          <div className='text-center'>
+            Hi,
+            <br />
+            I'm &nbsp;
+            <span className='font-jersey text-[30px] text-yellow-500'>
+              Harsh Master
+            </span>
+          </div>
+          <div id="skills" className='flex flex-row flex-wrap font-jersey w-fit'></div>
         </div>
         <img src={"/profile pic.png"} className='h-[250px] w-[250px] max-md:h-[30%] max-md:w-[30%]' alt="" />
       </div>
-
       <TypingAnimation onAnimationComplete={handleAnimationComplete} />
-
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -1,91 +1,70 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const TypingAnimation = ({ onAnimationComplete }) => {
-
-  const [input, setInput] = useState("")
-  const [fadeIn, setFadein] = useState(false)
-  const [typingAnimationComplete, setTypingAnimationComplete] = useState(false)
-  const [initialCoordinates, setinitialCoordinates] = useState({ x: 0, y: 0 });
-  const [skills, setSkills] = useState(["Android Developer"])
-  const [popId, setPopId] = useState("")
-  const skillIndex = useRef(0)
-  const index = useRef(0)
+  const [input, setInput] = useState('');
+  const [fadeIn, setFadein] = useState(false);
+  const [typingAnimationComplete, setTypingAnimationComplete] = useState(false);
+  const [skills, setSkills] = useState(["Android Developer", "Web Developer", "Python", "Javascript", "Machine Learning"]);
+  const [popId, setPopId] = useState('');
+  const skillIndex = useRef(0);
+  const index = useRef(0);
 
   const handleInputChange = (e) => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
+
   useEffect(() => {
     if (fadeIn && typingAnimationComplete) {
       const pop = () => {
         const popDiv = document.getElementById(popId);
         popDiv.innerText = input;
-        let rect = popDiv.getBoundingClientRect();
-
-        setinitialCoordinates({
-          x: rect.x,
-          y: rect.y
-        })
-
-        setTypingAnimationComplete(false)
-        setInput("")
-        index.current=0;
-        skillIndex.current++;
+        setTypingAnimationComplete(false);
+        setInput('');
+        onAnimationComplete({ popId });
       };
       pop();
     }
-  }, [typingAnimationComplete]);
-
-  useEffect(()=> {if (popId!=="") {
-    onAnimationComplete({ newCoordinates: initialCoordinates, popId: popId })
-  }}, [initialCoordinates])
+  }, [fadeIn, typingAnimationComplete]);
 
   useEffect(() => {
-    // console.log(skills.at(skillIndex), skillIndex.current)
-    if (fadeIn && !typingAnimationComplete && index.current==0 && skillIndex.current<skills.length) {
-      // const typingText = "Android Developer";
-      setPopId(uuidv4())
-      let typingText = skills.at(skillIndex.current);
-      const typingInterval = setInterval(() => {
-        setInput(typingText.substring(0, index.current + 1));
-        index.current++;
-        if (index.current === typingText.length) {
-          clearInterval(typingInterval)
-          setTypingAnimationComplete(true)
-        };
-      }, 100);
-      return () => {
-        clearInterval(typingInterval, typingAnimationComplete)
-      };
+    if (fadeIn && !typingAnimationComplete && index.current === 0 && skillIndex.current < skills.length) {
+      setPopId(uuidv4());
+      let typingText = skills[skillIndex.current];
+      setTimeout(() => {
+        const typingInterval = setInterval(() => {
+          setInput(typingText.substring(0, index.current + 1));
+          index.current++;
+          if (index.current === typingText.length) {
+            clearInterval(typingInterval);
+            setTypingAnimationComplete(true);
+            index.current = 0; // Reset index for the next word
+            skillIndex.current++; // Move to the next word
+          }
+        }, 100);
+      }, 1500)
+      // return () => {
+      //   clearInterval(typingInterval);
+      // };
     }
-  }, [fadeIn, skillIndex.current])
+  }, [fadeIn, typingAnimationComplete]);
 
-  // useEffect(()=>{
-  //   console.log(popId);
-  // }, [popId])
+  useEffect(() => {
+    if (popId !== "") {
+      var div = document.getElementById('popup'),
+        clone = div.cloneNode(true); // true means clone all childNodes and all event handlers
+      clone.id = popId;
+      document.getElementById("typeTextDiv").appendChild(clone);
+    }
+  }, [popId])
 
   return (
-    <div id='hi2' className='flex flex-row w-[20%] min-w-[200px] relative border-2 rounded-lg px-2 py-1 animate-fade' onAnimationEnd={() => { setFadein(true)}}>
-      <input type="text" className='w-full bg-transparent outline-none' onChange={handleInputChange} value={input} autoFocus/>
+    <div id="typeTextDiv" className='flex flex-row w-[20%] min-w-[200px] relative border-2 rounded-lg px-2 py-1 animate-fade' onAnimationEnd={() => setFadein(true)}>
+      <input type="text" className='font-jersey w-full bg-transparent outline-none' onChange={handleInputChange} value={input} autoFocus />
       <span>↵&#9166;</span>
-      <div id={popId} className='absolute'></div>
-      {/* {console.log(popId)} */}
+      <div id="popup" className='absolute font-jersey'></div>
     </div>
-  )
-}
+  );
+};
 
-export default TypingAnimation
-
-// const movingDiv = document.getElementById('hi2');
-// const stati = document.getElementById('hi');
-
-// // Initial and final positions
-// const initialPos = { x: movingDiv.getBoundingClientRect().x, y: hi2.getBoundingClientRect().y }; // Assuming initial position (150px, 100px) relative to container1
-// const finalPos = { x: stati.getBoundingClientRect().x, y: stati.getBoundingClientRect().y+40 }; // Assuming final position (150px, 100px) relative to container2
-
-// // Calculate translation values
-// const dx = finalPos.x - initialPos.x;
-// const dy = finalPos.y - initialPos.y;
-
-// // Apply transformation to move the div to the final position
-// movingDiv.style.transform = `translate(${dx}px, ${dy}px)`;
+export default TypingAnimation;
