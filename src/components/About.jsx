8c417bfc0f2e5e666d86e3257f0322a7ba5f9258
Harsh0707005 <harshmaster07705@gmail.com>
2024-07-02 17:17:34 +0530

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className='bg-[#333333] text-white min-h-[500px]'>
-      <h1>About</h1>
-      <div className='flex flex-col gap-[5px]'>
+    <section className='bg-white text-black flex flex-col justify-center items-center gap-[15px] p-[30px] text-center'>
+      <p className='font-bold text-2xl'>About Me</p>
+      <div className='flex flex-col gap-[10px] w-[85%]'>
 
-      <span>I started my programming journey around 3 years back with Python with the motive to develop 2D games using Pygame, soon this hobby turned into a passion, pursuing which gave me this incredible sense of fulfilment and satisfaction. Following which, I continued to explore about App and Web development and learnt languages like Java, HTML, CSS, Javascript, React JS and backend Databases like MongoDB.</span>
+        <span>I started my programming journey around 3 years back with Python with the motive to develop 2D games using Pygame, soon this hobby turned into a passion, pursuing which gave me this incredible sense of fulfilment and satisfaction. Following which, I continued to explore about App and Web development and learnt languages & libraries like Java, HTML, CSS, Javascript, React JS and backend Databases like MongoDB.</span>
 
-      <span>As a first-year data science student at IIT Madras, I'm thrilled to explore my passions and expand my skills in the tech industry. With a strong focus on programming and a keen interest in cybersecurity, I'm constantly seeking new challenges to enhance my knowledge.</span>
+        <span>As a second-year data science student at IIT Madras, I'm thrilled to explore my passions and expand my skills in the tech industry. With a strong focus on programming and a keen interest in cybersecurity, I'm constantly seeking new challenges to enhance my knowledge.</span>
 
-      <span>As a self-taught programmer, I've honed my coding skills through various projects and challenges. My curiosity about cyber security has led me to explore various concepts and techniques due to which even small vulnerabilities can lead to exploitation of application. This helps me to develop secure applications to prevent any potential vulnerabilities.</span>
+        <span>As a self-taught programmer, I've honed my coding skills through various projects and challenges. My curiosity about cyber security has led me to explore various concepts and techniques due to which even small vulnerabilities can lead to exploitation of application. This helps me to develop secure applications to prevent any potential vulnerabilities.</span>
 
-      <span>I also have a passion for writing about Cybersecurity and Ethical Hacking. I have written numerous articles on these topics. As a result, I was accepted as a Technical Content Writer at GeeksforGeeks, and my work has been featured in Infosec Write-ups, one of Medium's largest publications on cyber security. I'm always eager to share my knowledge and learn from others in the industry.</span>
+        <span>I also have a passion for writing about Cybersecurity and Ethical Hacking. I have written numerous articles on these topics. As a result, I was accepted as a Technical Content Writer at GeeksforGeeks, and my work has been featured in Infosec Write-ups, one of Medium's largest publications on cyber security. I'm always eager to share my knowledge and learn from others in the industry.</span>
 
-      <span>With my passion for technology and diverse skillset, I'm confident I can bring value to any project or team I am a part of. I am excited to collaborate with other like-minded individuals and contribute to advancing the tech industry.</span>
+        <span>With my passion for technology and diverse skillset, I'm confident I can bring value to any project or team I am a part of. I am excited to collaborate with other like-minded individuals and contribute to advancing the tech industry.</span>
       </div>
     </section>
   )
